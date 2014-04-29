fe95874357038afb1c9248d1754a9e33ca856e61
sgajaejung <sgajaejung@gmail.com>
2014-04-29 14:00:38 +0900

    float test

---
+++ 2강/floatTest/floatTest/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	float f1 = 10000000000.111f; // 소수점 이하 사라짐.
+	float f2 = 100.111f;
+	float f3 = 0.1; // warning 발생.
+
+	printf( "%f\n", f1);
+	printf( "%f\n", f2);
+	printf( "%f\n", f3);
+
+
+	cout << f1 << endl;
+	cout << f2 << endl;
+	cout << f3 << endl;
+}
