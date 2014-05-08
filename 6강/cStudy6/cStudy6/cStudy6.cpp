377a8d7378f48c6ab541c052d65c0762a3b33128
Lee JaeJung <sgajaejung@gmail.com>
2014-05-08 19:22:05 +0900

    update lecture6

--- 6강/cStudy6/cStudy6/cStudy6.cpp
+++ 6강/cStudy6/cStudy6/cStudy6.cpp
@@ -3,15 +3,37 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "hello world" << endl;
-	
 
+/*
+	cout << "login id : ";
+	string str;
+	cin >> str;
+
+	cout << "password : ";
+	string pw;
+	cin >> pw;
+
+//	cout << str << endl;
+//	cout << pw << endl;
+
+	if (str == "abc")
+	{
+		cout << "login ok" << endl;
+	}
+
+	string ss;
+	ss += "a";
+	ss += "cc";
+	cout << ss << endl;
+
+/**/
 
 	return 0;
 }
-
