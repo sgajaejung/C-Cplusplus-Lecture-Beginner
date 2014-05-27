4469182d2822ae3e584a2ac24752ae5f61bc162c
sgajaejung <sgajaejung@gmail.com>
2014-05-27 18:09:51 +0900

    update for5

---
+++ 17강/For1/for5/For5.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	for (int i=1; i < 20; i+=2)
+	{
+		int n = (20 - i)/2;
+		for (int k=0; k < n; ++k)
+		{
+			cout << " ";
+		}
+
+		for (int k=0; k < i; ++k)
+		{
+			cout << "*";
+		}
+
+		for (int k=0; k < n; ++k)
+		{
+			cout << " ";
+		}
+
+		cout << endl;
+	}
+
+	for (int i=20-1; i>= 0; i-=2)
+	{
+		int n = (20 - i)/2;
+		for (int k=0; k < n; ++k)
+		{
+			cout << " ";
+		}
+
+		for (int k=0; k < i; ++k)
+		{
+			cout << "*";
+		}
+
+		for (int k=0; k < n; ++k)
+		{
+			cout << " ";
+		}
+
+		cout << endl;
+	}
+
+}
+
